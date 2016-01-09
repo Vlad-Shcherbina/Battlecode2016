e3d8d8a4471adf544db65b27b829a55d8b9d31d5
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-01-09 22:34:44 +0100

    scout (disabled) (crappy, buggy, with lots of copy-paste)

--- src/turretblob/RobotPlayer.java
+++ src/turretblob/RobotPlayer.java
@@ -13,40 +13,88 @@ public class RobotPlayer {
     static Random rand;
     static RobotController rc;
 
+    static boolean knowTopBorder = false;
+    static boolean knowBottomBorder = false;
+    static boolean knowLeftBorder = false;
+    static boolean knowRightBorder = false;
+    static int topBorder;
+    static int bottomBorder;
+    static int leftBorder;
+    static int rightBorder;
+
     static class Comm {
+        static final int TARGET = 0;
+        static final int SCOUT_REPORT = 1;
+
+        static final int W2 = GameConstants.MAP_MAX_WIDTH * 2;
+
         static void reportTarget(RobotInfo target) throws Exception {
             int dx = target.location.x - rc.getLocation().x;
             int dy = target.location.y - rc.getLocation().y;
-            System.out.println("reporting target " + dx + " " + dy);
-            rc.broadcastMessageSignal(dx, dy, 2 * rc.getType().sensorRadiusSquared);
+            int data = dx + GameConstants.MAP_MAX_WIDTH +
+                      (dy + GameConstants.MAP_MAX_HEIGHT) * W2;
+            rc.setIndicatorLine(rc.getLocation(), target.location, 255, 255, 0);
+            rc.broadcastMessageSignal(TARGET, data, 2 * rc.getType().sensorRadiusSquared);
         }
 
-        static ArrayList<MapLocation> getTargets() throws Exception {
-            ArrayList<MapLocation> result = new ArrayList();
+        static void reportScoutExistence() throws Exception {
+            int d = GameConstants.MAP_MAX_WIDTH * GameConstants.MAP_MAX_WIDTH +
+                    GameConstants.MAP_MAX_HEIGHT * GameConstants.MAP_MAX_HEIGHT;
+            lastHeardFromScout = currentRound;
+            rc.broadcastMessageSignal(SCOUT_REPORT, 42, d);
+        }
+
+        static void processData() {
+            targets.clear();
             for (Signal signal : rc.emptySignalQueue()) {
                 if (signal.getTeam() == rc.getTeam()) {
                     int[] data = signal.getMessage();
                     if (data != null) {
-                        MapLocation pos = signal.getLocation();
-                        result.add(pos.add(data[0], data[1]));
+                        if (data[0] == TARGET) {
+                            MapLocation pos = signal.getLocation();
+                            int dx = data[1] % W2;
+                            int dy = data[1] / W2;
+                            dx -= GameConstants.MAP_MAX_WIDTH;
+                            dy -= GameConstants.MAP_MAX_HEIGHT;
+                            targets.add(pos.add(dx, dy));
+                        } else if (data[0] == SCOUT_REPORT) {
+                            lastHeardFromScout = currentRound;
+                        }
                     }
                 } else {
                     // TODO: take unit type and uncertainty into account
-                    result.add(signal.getLocation());
+                    targets.add(signal.getLocation());
                 }
             }
-            return result;
+        }
+
+        static ArrayList<MapLocation> targets = new ArrayList();
+        static int lastHeardFromScout = -1000;
+        final static int SCOUT_REPORT_PERIOD = 150;
+
+        static ArrayList<MapLocation> getTargets() throws Exception {
+            return targets;
         }
     }
 
     public static void run(RobotController rc_) {
         rc = rc_;
         rand = new Random(rc.getID());
+
         try {
+            int x = rc.getLocation().x;
+            int y = rc.getLocation().y;
+            topBorder = y - GameConstants.MAP_MAX_HEIGHT;
+            bottomBorder = y + GameConstants.MAP_MAX_HEIGHT;
+            leftBorder = x - GameConstants.MAP_MAX_WIDTH;
+            rightBorder = x + GameConstants.MAP_MAX_WIDTH;
+
             if (rc.getType() == RobotType.ARCHON) {
                 archonRun();
             } else if (rc.getType() == RobotType.TURRET) {
                 turretRun();
+            } else if (rc.getType() == RobotType.SCOUT) {
+                scoutRun();
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -54,11 +102,78 @@ public class RobotPlayer {
         }
     }
 
-    static void archonRun() throws Exception {
-        boolean repairedThisTurn = false;
+    static void yield() {
+        Clock.yield();
+        currentRound++;
+        Comm.processData();
+        repairedThisTurn = false;
+    }
+
+    static int currentRound = 0;
+    static boolean repairedThisTurn = false;
+
+    static void scoutRun() throws  Exception {
         while (true) {
-            if (rand.nextInt(5) == 0 && tryBuild(RobotType.TURRET))
+            if (!rc.isCoreReady()) {
+                yield();
                 continue;
+            }
+
+            if (Comm.lastHeardFromScout < currentRound - Comm.SCOUT_REPORT_PERIOD) {
+                Comm.reportScoutExistence();
+                yield();
+                continue;
+            }
+
+            RobotInfo[] allies = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared, rc.getTeam());
+            RobotInfo[] enemies = senseEnemies(rc.getType().sensorRadiusSquared);
+
+            int cnt = 0;
+            for (RobotInfo enemy : enemies) {
+                if (cnt > 10)
+                    break;
+                Comm.reportTarget(enemy);
+            }
+
+            // diagonal
+            Direction dir = directions[rand.nextInt(4) * 2 + 1];
+
+            if (rc.isCoreReady() && rc.canMove(dir)) {
+                int oldDist = Utils.distToNearest(rc.getLocation(), allies);
+                int newDist = Utils.distToNearest(rc.getLocation().add(dir), allies);
+
+                if (newDist < 10 || newDist <= oldDist) {
+                    rc.move(dir);
+                    continue;
+                }
+
+                rc.move(dir);
+                yield();
+                continue;
+            }
+
+            yield();
+        }
+    }
+
+    static void archonRun() throws Exception {
+        System.out.println(rc.getLocation().x + " " + rc.getLocation().y);
+        while (true) {
+            /*
+            // Scout does not seem to be useful
+            if (rand.nextInt(50) == 0 &&
+                Comm.lastHeardFromScout < currentRound - Comm.SCOUT_REPORT_PERIOD) {
+                if (tryBuild(RobotType.SCOUT)) {
+                    System.out.println("Building scout");
+                    yield();
+                    continue;
+                }
+            }*/
+
+            if (rand.nextInt(15) == 0 && tryBuild(RobotType.TURRET)) {
+                yield();
+                continue;
+            }
 
             RobotInfo[] allies = rc.senseNearbyRobots(50, rc.getTeam());
             RobotInfo[] enemies = senseEnemies(50);
@@ -76,18 +191,6 @@ public class RobotPlayer {
                 }
             }
 
-            int oldDist = Utils.distToNearest(rc.getLocation(), allies);
-
-            Direction dirToMove = directions[rand.nextInt(8)];
-            if (rc.isCoreReady() && rc.canMove(dirToMove)) {
-                int newDist = Utils.distToNearest(rc.getLocation().add(dirToMove), allies);
-
-                if (newDist < 10 || newDist <= oldDist) {
-                    rc.move(dirToMove);
-                    continue;
-                }
-            }
-
             int cnt = 0;
             for (RobotInfo enemy : enemies) {
                 if (cnt > 10)
@@ -95,8 +198,19 @@ public class RobotPlayer {
                 Comm.reportTarget(enemy);
             }
 
-            Clock.yield();
-            repairedThisTurn = false;
+            int oldDist = Utils.distToNearest(rc.getLocation(), allies);
+
+            Direction dir = directions[rand.nextInt(8)];
+            if (rc.isCoreReady() && rc.canMove(dir)) {
+                int newDist = Utils.distToNearest(rc.getLocation().add(dir), allies);
+
+                if (newDist < 10 || newDist <= oldDist) {
+                    rc.move(dir);
+                    continue;
+                }
+            }
+
+            yield();
         }
     }
 
@@ -109,7 +223,7 @@ public class RobotPlayer {
     static void turretRun() throws Exception {
         while (true) {
             if (!rc.isWeaponReady()) {
-                Clock.yield();
+                yield();
                 continue;
             }
 
@@ -135,10 +249,9 @@ public class RobotPlayer {
 
             if (target != null) {
                 rc.attackLocation(target);
-                continue;
             }
 
-            Clock.yield();
+            yield();
         }
     }
 
