fae661e3c03dc2366052ca904c1e813fe19efe88
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2016-01-09 19:42:51 +0100

    spotting

--- src/turretblob/RobotPlayer.java
+++ src/turretblob/RobotPlayer.java
@@ -2,6 +2,7 @@ package turretblob;
 
 import battlecode.common.*;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 @SuppressWarnings("unused")
@@ -11,6 +12,32 @@ public class RobotPlayer {
             Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
     static Random rand;
     static RobotController rc;
+
+    static class Comm {
+        static void reportTarget(RobotInfo target) throws Exception {
+            int dx = target.location.x - rc.getLocation().x;
+            int dy = target.location.y - rc.getLocation().y;
+            System.out.println("reporting target " + dx + " " + dy);
+            rc.broadcastMessageSignal(dx, dy, 2 * rc.getType().sensorRadiusSquared);
+        }
+
+        static ArrayList<MapLocation> getTargets() throws Exception {
+            ArrayList<MapLocation> result = new ArrayList();
+            for (Signal signal : rc.emptySignalQueue()) {
+                if (signal.getTeam() == rc.getTeam()) {
+                    int[] data = signal.getMessage();
+                    if (data != null) {
+                        MapLocation pos = signal.getLocation();
+                        result.add(pos.add(data[0], data[1]));
+                    }
+                } else {
+                    // TODO: take unit type and uncertainty into account
+                    result.add(signal.getLocation());
+                }
+            }
+            return result;
+        }
+    }
 
     public static void run(RobotController rc_) {
         rc = rc_;
@@ -33,6 +60,8 @@ public class RobotPlayer {
                 continue;
 
             RobotInfo[] allies = rc.senseNearbyRobots(50, rc.getTeam());
+            RobotInfo[] enemies = senseEnemies(50);
+
             int oldDist = Utils.distToNearest(rc.getLocation(), allies);
 
             Direction dirToMove = directions[rand.nextInt(8)];
@@ -45,8 +74,21 @@ public class RobotPlayer {
                 }
             }
 
+            int cnt = 0;
+            for (RobotInfo enemy : enemies) {
+                if (cnt > 10)
+                    break;
+                Comm.reportTarget(enemy);
+            }
+
             Clock.yield();
         }
+    }
+
+    static RobotInfo[] senseEnemies(int range) throws Exception {
+        RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(range, rc.getTeam().opponent());
+        RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(range, Team.ZOMBIE);
+        return Utils.concatArrays(enemiesWithinRange, zombiesWithinRange);
     }
 
     static void turretRun() throws Exception {
@@ -57,21 +99,27 @@ public class RobotPlayer {
             }
 
             int myAttackRange = rc.getType().attackRadiusSquared;
-            RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, rc.getTeam().opponent());
-            RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
 
-            RobotInfo[] enemies = Utils.concatArrays(enemiesWithinRange, zombiesWithinRange);
-            RobotInfo target = null;
-
-            for (RobotInfo enemy : enemies) {
+            MapLocation target = null;
+            for (RobotInfo enemy : senseEnemies(myAttackRange)) {
                 if (rc.getLocation().distanceSquaredTo(enemy.location) >=
                     GameConstants.TURRET_MINIMUM_RANGE) {
-                    target = enemy;
+                    target = enemy.location;
                     break;
                 }
             }
+            for (MapLocation t : Comm.getTargets()) {
+                if (target != null)
+                    continue;
+                int d = rc.getLocation().distanceSquaredTo(t);
+                if (d >= GameConstants.TURRET_MINIMUM_RANGE &&
+                    d <= myAttackRange) {
+                    target = t;
+                }
+            }
+
             if (target != null) {
-                rc.attackLocation(target.location);
+                rc.attackLocation(target);
                 continue;
             }
 
